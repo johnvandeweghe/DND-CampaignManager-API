6bc2014e9a3e7810ce44ad86a3f428950f44d017
John VanDeWeghe <johnvandeweghe@users.noreply.github.com>
2017-02-14 19:20:05 -0600

    Update initial.sql

--- sql/initial.sql
+++ sql/initial.sql
@@ -44,14 +44,44 @@ CREATE TABLE player_characters (
 CREATE TABLE items (
     id INT(11),
     entry_id INT(11),
-    low_value varchar(32),
-    fair_value varchar(32),
-    high_value varchar(32),
-    effects text,
-
+    low_value VARCHAR(32),
+    fair_value VARCHAR(32),
+    high_value VARCHAR(32),
+    effects TEXT,
     UNIQUE KEY (entry_id),
     FOREIGN KEY (entry_id)
         REFERENCES entries (id)
         ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE locations (
+    id INT(11),
+    entry_id INT(11),
+    UNIQUE KEY (entry_id),
+    FOREIGN KEY (entry_id)
+        REFERENCES entries (id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE monsters (
+    id INT(11),
+    entry_id INT(11),
+    max_hit_points INT(11),
+    constitution INT(11),
+    strength INT(11),
+    dexterity INT(11),
+    intelligence INT(11),
+    wisdom INT(11),
+    charisma INT(11),
+    type VARCHAR(32),
+    alignment ENUM('lawful-good', 'lawful-neutral', 'lawful-evil', 'neutral-good', 'neutral-neutral', 'neutral-evil', 'chaotic-good', 'chaotic-neutral', 'chaotic-evil'),
+    features TEXT,
+    attacks TEXT,
+    UNIQUE KEY (entry_id),
+    FOREIGN KEY (entry_id)
+        REFERENCES entries (id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (id)
+);
